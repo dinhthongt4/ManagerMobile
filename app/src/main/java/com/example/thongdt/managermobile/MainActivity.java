037e429977e14547be1af0a60bc4cc6477fd1286
ThongTD <dinhthongt4@gmail.com>
2015-10-07 10:59:16 +0700

    Initial commit

---
+++ app/src/main/java/com/example/thongdt/managermobile/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.thongdt.managermobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity {
+
+    @AfterViews
+    void init() {
+
+    }
+}
